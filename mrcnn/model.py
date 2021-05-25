0b8631025a0515deb56036ecdd841dda03427e98
Erik Vroon <erik.vroon22@gmail.com>
2021-05-25 16:26:10 +0200

    Fix hanging tf at starting first epoch

--- mrcnn/model.py
+++ mrcnn/model.py
@@ -2365,7 +2365,7 @@ class MaskRCNN(object):
             validation_steps=self.config.VALIDATION_STEPS,
             max_queue_size=100,
             workers=workers,
-            use_multiprocessing=workers > 1,
+            use_multiprocessing=False,
         )
         self.epoch = max(self.epoch, epochs)
 
